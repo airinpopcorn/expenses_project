eee70aae95e3d53d11ce1480829a323a5744d3cb
Irene Alonso <i.alonso@accenture.com>
2022-12-22 18:02:17 +0100

    Add ListTile widget beside card expenses

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,7 +4,7 @@ import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
-  TransactionList(this.transactions);
+  const TransactionList(this.transactions, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -32,40 +32,27 @@ class TransactionList extends StatelessWidget {
             : ListView.builder(
                 itemBuilder: (context, index) {
                   return Card(
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 15),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Theme.of(context).primaryColor,
-                              width: 2,
-                            ),
+                    elevation: 5,
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: FittedBox(
+                            child: Text('€${transactions[index].amount}'),
                           ),
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                              "€ ${transactions[index].amount.toStringAsFixed(2)}",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Theme.of(context).primaryColor)),
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              transactions[index].title as String,
-                              style: Theme.of(context).textTheme.titleMedium,
-                            ),
-                            Text(
-                              DateFormat.yMMMd()
-                                  .format(transactions[index].date),
-                              style: TextStyle(color: Colors.grey),
-                            )
-                          ],
-                        )
-                      ],
+                      ),
+                      title: Text(
+                        transactions[index].title,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      subtitle: Text(
+                        DateFormat.yMMMd().format(transactions[index].date),
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
                     ),
                   );
                 },
